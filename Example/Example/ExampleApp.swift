c8ac8c67bef43a0146f329e9372d832905a8d64d
Dan Berglund <dan@berglund.io>
2021-05-12 21:15:12 +0200

    Get something super-basic up and running, only support for a single Button or Text right now

--- Example/Example/ExampleApp.swift
+++ Example/Example/ExampleApp.swift
@@ -8,24 +8,36 @@
 import SwiftUI
 import SwiftELM
 
+struct Model {
+    let text: String
+}
+
+enum Message {
+    case click
+}
+
+let app = SwiftELM.sandbox(
+    initial: Model(text: "Hello world"),
+    view: { model in
+        Button(Text(model.text), onClick: Message.click)
+    },
+    update: { (message: Message, model: Model) in
+        .init(text: String(model.text.dropLast()))
+    }
+)
+
 
 @main
 struct ExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            app.body
         }
-    }
-}
-
-struct ContentView: View {
-    var body: some View {
-        Text(SwiftELM.text)
     }
 }
 
 struct ContentViewPreviewProvider: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        app.body
     }
 }
--- Sources/SwiftELM/SwiftELM.swift
+++ Sources/SwiftELM/SwiftELM.swift
@@ -1,3 +1,78 @@
+import SwiftUI
+
+public protocol Component {
+    associatedtype Message
+    associatedtype Content: SwiftUI.View
+
+    var body: Content { get }
+    var sink: ((Message) -> Void)? { get set }
+}
+
+public struct Text: Component {
+    public typealias Message = Never
+    public let body: SwiftUI.Text
+    public init(_ text: String) {
+        body = SwiftUI.Text(text)
+    }
+    public var sink: ((Message) -> Void)?
+}
+
+public struct Button<Message, Label: View>: Component {
+    let content: Label
+    let message: Message
+    public init(_ content: Label, onClick message: Message) {
+        self.content = content
+        self.message = message
+    }
+
+    public var body: some View {
+        SwiftUI.Button(action: { sink?(message) }, label: { content })
+    }
+    public var sink: ((Message) -> Void)?
+}
+
+public struct Sandbox<Model, C: Component> {
+    private let stateContainer: StateContainer
+    private let view: (Model) -> C
+    private let update: (C.Message, Model) -> Model
+
+    init(initial: Model, view: @escaping (Model) -> C, update: @escaping (C.Message, Model) -> Model) {
+        stateContainer = .init(model: initial)
+        self.view = view
+        self.update = update
+    }
+
+    public var body: some View {
+        Content(stateContainer: stateContainer, transform: { model in
+            var view = view(model)
+            view.sink = { message in
+                stateContainer.model = update(message, model)
+            }
+
+            return view.body
+        })
+    }
+
+    class StateContainer: ObservableObject {
+        @Published var model: Model
+
+        init(model: Model) {
+            self.model = model
+        }
+    }
+
+    struct Content: View {
+        @ObservedObject var stateContainer: StateContainer
+        let transform: (Model) -> C.Content
+
+        var body: some View {
+            transform(stateContainer.model)
+        }
+    }
+}
+
 public struct SwiftELM {
-    public static var text = "Hello, World!"
+    public static func sandbox<Model, C: Component>(initial: Model, view: @escaping (Model) -> C, update: @escaping (C.Message, Model) -> Model) -> Sandbox<Model, C> {
+        Sandbox(initial: initial, view: view, update: update)
+    }
 }
