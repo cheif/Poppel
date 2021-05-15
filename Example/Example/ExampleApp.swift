2b55322cc1c2c0d1f41c97c046f4c7496ce0eee8
Dan Berglund <dan@berglund.io>
2021-05-15 19:09:15 +0200

    Support for if/else when building

--- Example/Example/ExampleApp.swift
+++ Example/Example/ExampleApp.swift
@@ -11,32 +11,43 @@ import SwiftELM
 struct App: ElmApp {
     struct Model {
         let number: Int
+        let showOther: Bool
     }
 
     enum Message {
         case increase
         case decrease
+        case other
     }
 
     let container = SwiftELM.sandbox(
-        initial: .init(number: 10),
+        initial: .init(number: 10, showOther: false),
         view: { model in
-            VStack(spacing: 30) {
-                Button(onClick: Message.increase) {
-                    Text("Increase")
-                }
-                Text<Message>("Current: \(model.number)")
-                Button(onClick: Message.decrease) {
-                    Text("Decrease")
+                VStack(spacing: 30) {
+                    if model.showOther {
+                        Text<Message>("This is other!")
+                    } else {
+                        Button(onClick: Message.increase) {
+                            Text("Increase")
+                        }
+                        Text<Message>("Current: \(model.number)")
+                        Button(onClick: Message.decrease) {
+                            Text("Decrease")
+                        }
+                        Button(onClick: Message.other) {
+                            Text("Switch")
+                        }
                 }
             }
         },
         update: { (message: Message, model: Model) in
             switch message {
             case .increase:
-                return .init(number: model.number + 1)
+                return .init(number: model.number + 1, showOther: model.showOther)
             case .decrease:
-                return .init(number: model.number - 1)
+                return .init(number: model.number - 1, showOther: model.showOther)
+            case .other:
+                return .init(number: model.number, showOther: true)
             }
         }
     )
