933fdd3a0180f0d6f1f1e10bd06434268a39cae7
Dan Berglund <dan@berglund.io>
2021-05-16 13:17:19 +0200

    Add support for commands, to do async-work

--- Example/Example/ExampleApp.swift
+++ Example/Example/ExampleApp.swift
@@ -5,26 +5,40 @@
 //  Created by Dan Berglund on 2021-05-12.
 //
 
+import Foundation
 import SwiftELM
+
+extension URLSession {
+    public func command<Message>(with url: URL, transform: @escaping (Data?, URLResponse?, Error?) -> Message) -> Command<Message> {
+        return { callback in
+            self
+                .dataTask(with: url, completionHandler: { callback(transform($0, $1, $2)) })
+                .resume()
+        }
+    }
+}
 
 @main
 struct App: ElmApp {
     struct Model {
         let number: Int
         let showOther: Bool
+        let text: String?
     }
 
     enum Message {
         case increase
         case decrease
         case other
+        case gotText(String?)
     }
 
-    let container = SwiftELM.sandbox(
-        initial: .init(number: 10, showOther: false),
+    let container = SwiftELM.app(
+        initial: .init(number: 10, showOther: false, text: nil),
         view: { model in
             if model.showOther {
                 Text<Message>("This is other!")
+                Text<Message>(model.text ?? "Loading...")
             } else {
                 VStack(spacing: 30) {
                     Button(onClick: Message.increase) {
@@ -43,11 +57,16 @@ struct App: ElmApp {
         update: { (message: Message, model: Model) in
             switch message {
             case .increase:
-                return .init(number: model.number + 1, showOther: model.showOther)
+                return (.init(number: model.number + 1, showOther: model.showOther, text: model.text), { _ in})
             case .decrease:
-                return .init(number: model.number - 1, showOther: model.showOther)
+                return (.init(number: model.number - 1, showOther: model.showOther, text: model.text), { _ in})
             case .other:
-                return .init(number: model.number, showOther: true)
+                return (.init(number: model.number, showOther: true, text: model.text),
+                        URLSession.shared.command(with: URL(string: "https://elm-lang.org/assets/public-opinion.txt")!, transform: { data, _, _ in
+                            Message.gotText(data.flatMap { String(data: $0, encoding: .utf8) })
+                        }))
+            case .gotText(let text):
+                return (.init(number: model.number, showOther: true, text: text), { _ in})
             }
         }
     )
--- Sources/SwiftELM/SwiftELM.swift
+++ Sources/SwiftELM/SwiftELM.swift
@@ -112,35 +112,49 @@ public struct Button<Message, Label: Component>: Component where Label.Message =
     }
 }
 
-public struct AppContainer<Model, Content: Component>: View {
-    private let container: ModelContainer<Model>
-    private let view: (Model) -> Content
-    private let update: (Content.Message, Model) -> Model
+public typealias Command<Message> = (@escaping (Message) -> Void) -> Void
 
-    init(initial: Model, view: @escaping (Model) -> Content, update: @escaping (Content.Message, Model) -> Model) {
-        self.container = .init(model: initial)
+public struct AppContainer<Model, Message, Content: Component>: View where Content.Message == Message {
+    private let container: ModelContainer<Model, Message>
+    private let view: (Model) -> Content
+
+    init(initial: Model, command: @escaping Command<Message>, view: @escaping (Model) -> Content, update: @escaping (Content.Message, Model) -> (Model, Command<Message>)) {
+        self.container = .init(model: initial, update: update)
+        container.run(command)
         self.view = view
-        self.update = update
     }
 
     public var body: some View {
         RenderContainer(container: container, transform: { model in
-            view(model).render { message in
-                container.model = update(message, container.model)
-            }
+            view(model).render(container.send)
         })
     }
 
-    class ModelContainer<Model>: ObservableObject {
+    class ModelContainer<Model, Message>: ObservableObject {
         @Published var model: Model
+        let update: (Message, Model) -> (Model, Command<Message>)
 
-        init(model: Model) {
+        init(model: Model, update: @escaping (Message, Model) -> (Model, Command<Message>)) {
             self.model = model
+            self.update = update
+        }
+
+        func run(_ command: Command<Message>) {
+            #warning("Do we need some sort of syncronization here, since we might have multiple commands running?")
+            command(send)
+        }
+
+        func send(_ message: Message) {
+            let (model, command) = update(message, model)
+            DispatchQueue.main.async {
+                self.model = model
+            }
+            run(command)
         }
     }
 
     struct RenderContainer: View {
-        @ObservedObject var container: ModelContainer<Model>
+        @ObservedObject var container: ModelContainer<Model, Message>
         let transform: (Model) -> Content.Content
 
         var body: some View {
@@ -151,9 +165,10 @@ public struct AppContainer<Model, Content: Component>: View {
 
 @available(iOS 14.0, *)
 public protocol ElmApp: App {
-    associatedtype Content: Component
     associatedtype Model
-    var container: AppContainer<Model, Content> { get }
+    associatedtype Message
+    associatedtype Content: Component where Content.Message == Message
+    var container: AppContainer<Model, Message, Content> { get }
 }
 
 @available(iOS 14.0, *)
@@ -166,7 +181,20 @@ extension ElmApp {
 }
 
 public struct SwiftELM {
-    public static func sandbox<Model, C: Component>(initial: Model, @ComponentBuilder view: @escaping (Model) -> C, update: @escaping (C.Message, Model) -> Model) -> AppContainer<Model, C> {
-        AppContainer(initial: initial, view: view, update: update)
+    public static func sandbox<Model, Message, C: Component>(
+        initial: Model,
+        @ComponentBuilder view: @escaping (Model) -> C,
+        update: @escaping (Message, Model) -> Model
+    ) -> AppContainer<Model, Message, C> where C.Message == Message {
+        AppContainer(initial: initial, command: { _ in}, view: view, update: { message, model in (update(message, model), { _ in}) })
+    }
+
+    public static func app<Model, Message, C: Component>(
+        initial: Model,
+        command: @escaping Command<Message> = {_ in},
+        @ComponentBuilder view: @escaping (Model) -> C,
+        update: @escaping (Message, Model) -> (Model, Command<Message>)
+    ) -> AppContainer<Model, Message, C> where C.Message == Message {
+        AppContainer(initial: initial, command: command, view: view, update: update)
     }
 }
